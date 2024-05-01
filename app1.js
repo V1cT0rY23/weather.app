558e2b5aaba64484a5f817d3a192dece14be22fc
chibueze victory <chibuezedc4@gmail.com>
2024-05-01 23:37:58 +0100

    Update app1.js

--- app1.js
+++ app1.js
@@ -1,37 +1,32 @@
-
-const apiKey = " 1409a191044fa110fd667e27fdc7da00";
-const cityInput =  document.getElementById("cityInput");
+const apiKey = "1409a191044fa110fd667e27fdc7da00";
+const cityInput = document.getElementById("cityInput");
 const getWeatherButton = document.getElementById("getWeather");
 const alertsDiv = document.getElementById("alerts");
 
 getWeatherButton.addEventListener("click", () => {
-	const cityName = cityInput.value.trim();
-	if (cityName === "") {
-		alert("Please enter a city name.");
-		return;
-	}
-
-	const completeUrl = `(link unavailable);
-
-	fetch(completeUrl, {
-		type: 'GET'
-	})
-	.then((response) => response.json())
-	.then((data) => {
-		if (data.hasOwnProperty("alert") && data.alert !== null) {
-			const alertMessage = document.createElement("p");
-			alertMessage.textContent = data.alert.description;
-			alertsDiv.appendChild(alertMessage);
-		} else {
-			const noAlertsMessage = document.createElement("p");
-			noAlertsMessage.textContent = "No weather alerts at this time.";
-			alertsDiv.appendChild(noAlertsMessage);
-			getWeatherButton.disabled = true;
-			setTimeout(() => {
-				getWeatherButton.disabled = false;
-				alertsDiv.innerHTML = "";
-			}, 10000);
-		}
-	})
-	.catch((error) => console.error(error));
+  const cityName = cityInput.value.trim();
+  if (cityName === "") {
+    alert("Please enter a city name.");
+    return;
+  }
+  const completeUrl = `https://api.openweathermap.org/data/2.5/weather?q=${cityName}&appid=${apiKey}`;
+  fetch(completeUrl)
+    .then((response) => response.json())
+    .then((data) => {
+      if (data.hasOwnProperty("alert")) {
+        const alertMessage = document.createElement("p");
+        alertMessage.textContent = data.alert.description;
+        alertsDiv.appendChild(alertMessage);
+      } else {
+        const noAlertsMessage = document.createElement("p");
+        noAlertsMessage.textContent = "No weather alerts at this time.";
+        alertsDiv.appendChild(noAlertsMessage);
+        getWeatherButton.disabled = true;
+        setTimeout(() => {
+          getWeatherButton.disabled = false;
+          alertsDiv.innerHTML = "";
+        }, 10000);
+      }
+    })
+    .catch((error) => console.error(error));
 });
