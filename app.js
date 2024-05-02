bdf7f88a033540c90784456733c5cbc949398312
chibueze victory <chibuezedc4@gmail.com>
2024-05-02 11:38:45 +0100

    Update app.js.

--- app.js
+++ app.js
@@ -44,7 +44,7 @@ function showError(error) {
 }
 
 // GET WEATHER FROM API PROVIDER
-function getWeather(latitude, longitude) {
+const getWeather = (latitude, longitude) => {
   let api = `http://api.openweathermap.org/data/2.5/weather?lat=${latitude}&lon=${longitude}&appid=${key}`;
 
   fetch(api)
